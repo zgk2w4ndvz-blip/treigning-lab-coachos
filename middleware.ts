dc09a30223b37fb8f610202a507295160e2b6888
Frankie Perrelli <frankieperrelli@Mac.home.local>
2026-06-14 09:15:06 -0500

    Use Clerk frontend API matcher

--- middleware.ts
+++ middleware.ts
@@ -25,5 +25,6 @@ export const config = {
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run on API routes.
     "/(api|trpc)(.*)",
+    "/__clerk/(.*)",
   ],
 }
