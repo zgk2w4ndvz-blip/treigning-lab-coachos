f681d96f53304f456e2e52ad944ea36a82da0ed1
Frankie Perrelli <frankieperrelli@Mac.home.local>
2026-06-14 09:20:02 -0500

    Remove manual Clerk asset matcher

--- middleware.ts
+++ middleware.ts
@@ -25,6 +25,5 @@ export const config = {
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
     // Always run on API routes.
     "/(api|trpc)(.*)",
-    "/__clerk/(.*)",
   ],
 }
